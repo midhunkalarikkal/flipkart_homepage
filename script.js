f7f79cfc8e235e73aa7cd06b8025c787b6c2e02c
midhun k paniker <91378594+midhunkalarikkal@users.noreply.github.com>
2023-09-21 01:04:40 +0530

    Comments added

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-
+//Hide scroll bar without removing its function
 $(document).ready(function () {
     $('.scroll-arrow').on('click', function () {
         const direction = $(this).hasClass('left') ? '-=' : '+=';
